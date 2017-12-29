929c5a50186aedb1bf8c998b8b38380eb4132dac
Çağatay <cagatayldzz@gmail.com>
2017-12-29 13:33:56 +0300

    Create base.php

---
+++ messages/tr/base.php
@@ -0,0 +1,9 @@
+<?php
+
+	return [
+		'<strong>LinkedIn</strong>' => '<strong>LinkedIn</strong>',
+		'LinkedIn Settings' => 'LinkedIn Ayarlar',
+		'LinkedIn Widget URL:' => 'LinkedIn Widget URL:',
+		'<strong>LinkedIn</strong> module configuration' => '<strong>LinkedIn</strong> modül yapılandırması',
+		'Save' => 'Kaydet',
+	];
